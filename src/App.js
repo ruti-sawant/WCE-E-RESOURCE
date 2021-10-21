d8b34cf4023ee0282103ef972d1c1f4ed3226cbf
Harshal Kodgire <84096031+Harshal-9@users.noreply.github.com>
2021-10-21 12:38:31 +0530

    Initial frontend https://codesandbox.io/s/wce-e-resource-kig1f?file=/src/App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>WCE E-RESOURCE</h1>
+      <div className="div1">
+        <img
+          className="wceimg"
+          src="https://uni.wcoeapps.in/site/static/images/wcoe.jpg"
+          alt="wce"
+        />
+        <h1 className="heading">WCE E-RESOURCE</h1>
+      </div>
+      <hr />
+      <div className="mainButtons">
+        <button className="studentButton">Student Login</button>
+        <button className="facultyButton">Faculty Login</button>
+        <button className="TPOButton">TPO Login</button>
+      </div>
+      <footer>© Walchand College of Engineering, Sangli</footer>
     </div>
   );
 }
