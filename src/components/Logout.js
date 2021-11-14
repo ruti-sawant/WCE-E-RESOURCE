316c3c92a3562f409cfb2ed16bc63a4b2f0bf301
Harshal Kodgire <84096031+Harshal-9@users.noreply.github.com>
2021-11-15 00:14:31 +0530

    Handled logout https://codesandbox.io/s/modest-thunder-kijgm?file=/src/components/Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Logout() {
   return (
@@ -9,8 +10,24 @@ function Logout() {
       <div className="content" style={{ verticalAlign: "center" }}>
         <div className="logout">
           <h1>Are you sure you want to logout ? </h1>
-          <Link to="/Login">
-            <button className="logoutButton">Logout</button>
+          <Link to="/">
+            <button
+              className="logoutButton"
+              onClick={() => {
+                axios
+                  .get("https://afternoon-ocean-57702.herokuapp.com/logout", {
+                    withCredentials: true
+                  })
+                  .then((res) => {
+                    console.log(res);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              }}
+            >
+              Logout
+            </button>
           </Link>
           <Link to="/StudentPage">
             <button className="cancel">Cancel</button>
