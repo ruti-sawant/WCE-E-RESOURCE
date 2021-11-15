bfe494c0bd4474ed827e41b60744ddbceabe28c7
Harshal Kodgire <84096031+Harshal-9@users.noreply.github.com>
2021-11-15 20:17:23 +0530

    Handled adding new user, displaying profile according to user https://codesandbox.io/s/hardcore-dust-91g6u?file=/src/components/NewLoginDetails.js

--- src/components/NewLoginDetails.js
+++ src/components/NewLoginDetails.js
@@ -1,77 +1,147 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import Select from "react-select";
 import "./NewLoginDetails.css";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
 function LoginDetails() {
+  const [role, setRole] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(event.target.fName.value);
+    // console.log(event.target.PRN.value);
+
+    if (
+      event.target.selectedRole.value === "student" &&
+      event.target.PRN.value === ""
+    ) {
+      alert("Enter PRN for student!");
+      return;
+    }
+
+    const fd = new FormData();
+    fd.append("firstName", event.target.fName.value);
+    fd.append("middleName", event.target.mName.value);
+    fd.append("lastName", event.target.lName.value);
+    fd.append("PRN", event.target.PRN.value);
+    fd.append("email", event.target.email.value);
+    fd.append("contact", event.target.contact.value);
+    fd.append("department", event.target.department.value);
+    fd.append("role", event.target.selectedRole.value);
+
+    axios
+      .post("https://afternoon-ocean-57702.herokuapp.com/profile", fd)
+      .then((res) => {
+        console.log(res);
+        alert("User added successfully");
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err.message);
+        alert("Failed to add User");
+      });
+  }
+
+  useEffect(() => {
+    axios
+      .get("https://afternoon-ocean-57702.herokuapp.com/login", {
+        withCredentials: true
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.data.loggedIn === false) setRole("invalid");
+        //to get resources
+        else {
+          setRole(res.data.decodedData.role);
+        }
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err.message);
+        setRole("invalid");
+      });
+  }, []);
+
   return (
     <div>
-      <Sidebar />
-      <div className="content">
-        <div
-          // className="TPOPI"
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0.5px",
-            marginTop: "40px",
-            textAlign: "center",
-            marginLeft: "20%",
-            marginRight: "20%"
-          }}
-        >
-          <br />
-          <form style={{ textAlign: "center" }} type="post">
-            <label htmlFor="fName">Enter first Name : </label>
-            <input type="text" id="fName" required></input>
-            <br />
-            <br />
-            <label htmlFor="mName">Enter middle Name : </label>
-            <input type="text" id="mName"></input>
-            <br />
-            <br />
-            <label htmlFor="lName">Enter last Name : </label>
-            <input type="text" id="lName" required></input>
-            <br />
-            <br />
-            <label htmlFor="prn">Enter PRN : </label>
-            <input type="text"></input>
-            <br />
-            <br />
-            <label htmlFor="email">Enter Email : </label>
-            <input type="text" required></input>
-            <br />
-            <br />
-            <label htmlFor="contactNo">Enter Contact Number : </label>
-            <input
-              type="tel"
-              name="phone"
-              pattern="[0-9]{10}"
-              placeholder="10 digit number"
-            ></input>
-            <br />
-            <br />
-            <label htmlFor="branch ">Enter Department: </label>
-            <input type="text" required></input>
-            <br />
-            <br />
-            <label htmlFor="role">Select your role : </label>
-            <Select
-              id="selectRole"
-              className="selectRole"
-              required
-              options={[
-                { label: "student", value: "1" },
-                { label: "faculty", value: "2" },
-                { label: "TPO", value: "3" }
-              ]}
-            />
-            <br />
-            <br />
-            <button type="submit">Submit</button>
-            <br />
-            <br />
-          </form>
+      {role === "student" || role === "invalid" ? (
+        <Redirect to="/StudentPage/profile" />
+      ) : (
+        <div>
+          <Sidebar />
+          <div className="content">
+            <div
+              style={{
+                borderStyle: "solid",
+                borderWidth: "0.5px",
+                marginTop: "40px",
+                textAlign: "center",
+                marginLeft: "20%",
+                marginRight: "20%"
+              }}
+            >
+              <br />
+              <form style={{ textAlign: "center" }} onSubmit={handleSubmit}>
+                <label htmlFor="fName">Enter first Name : </label>
+                <input type="text" name="fName" id="fName" required></input>
+                <br />
+                <br />
+                <label htmlFor="mName">Enter middle Name : </label>
+                <input type="text" name="mName" id="mName"></input>
+                <br />
+                <br />
+                <label htmlFor="lName">Enter last Name : </label>
+                <input type="text" name="lName" id="lName" required></input>
+                <br />
+                <br />
+                <label htmlFor="prn">Enter PRN : </label>
+                <input type="text" name="PRN"></input>
+                <br />
+                <br />
+                <label htmlFor="email">Enter Email : </label>
+                <input type="text" name="email" required></input>
+                <br />
+                <br />
+                <label htmlFor="contactNo">Enter Contact Number : </label>
+                <input
+                  type="tel"
+                  name="contact"
+                  pattern="[0-9]{10}"
+                  placeholder="10 digit number"
+                ></input>
+                <br />
+                <br />
+                <label htmlFor="department ">Enter Department: </label>
+                <input type="text" name="department" required></input>
+                <br />
+                <br />
+                <label htmlFor="selectedRole">Select your role : </label>
+                <Select
+                  id="selectRole"
+                  className="selectRole"
+                  name="selectedRole"
+                  defaultValue={{ label: "Student", value: "student" }}
+                  options={[
+                    { label: "Student", value: "student" },
+                    { label: "Faculty", value: "faculty" },
+                    { label: "TPO", value: "TPO" }
+                  ]}
+                  required
+                />
+
+                <br />
+                <br />
+                <br />
+                <button type="submit">Submit</button>
+                <br />
+                <br />
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,125 +1,148 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
 import "./Profile.css";
 function Profile() {
+  const [role, setRole] = useState("");
+
+  const [userData, setUserData] = useState({
+    name: {
+      firstName: "",
+      middleName: "",
+      lastName: ""
+    },
+    username: "",
+    PRN: "",
+    email: "",
+    contact: "",
+    department: "",
+    numberOfResourceUploaded: 0
+  });
+  useEffect(() => {
+    axios
+      .get("https://afternoon-ocean-57702.herokuapp.com/login", {
+        withCredentials: true
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.data.loggedIn === false) setRole("invalid");
+        else {
+          setRole(res.data.decodedData.role);
+
+          axios
+            .get(
+              "https://afternoon-ocean-57702.herokuapp.com/profile/" +
+                res.data.decodedData.username
+            )
+            .then((data) => {
+              console.log("Recived data :", data);
+              setUserData(data.data);
+            })
+            .catch((err) => {
+              console.log("error");
+              console.log(err.message);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err.message);
+        setRole("invalid");
+      });
+  }, []);
+
   return (
     <div>
-      <Sidebar />
-      <div className="content">
-        <div className="profileImg" style={{ marginTop: "12px" }}>
-          <img
-            style={{ borderRadius: "50%", width: "250px" }}
-            src="https://uni.wcoeapps.in/site/static/images/wcoe.jpg"
-            alt="wce"
-          />
-          <hr />
+      {role === "invalid" ? (
+        <Redirect to="/login" />
+      ) : (
+        <div>
+          <Sidebar />
+          <div className="content">
+            <div className="profileImg" style={{ marginTop: "12px" }}>
+              <img
+                style={{ borderRadius: "50%", width: "250px" }}
+                src="https://uni.wcoeapps.in/site/static/images/wcoe.jpg"
+                alt="wce"
+              />
+              <hr />
+            </div>
+
+            <br />
+            <table style={{ width: "100%" }}>
+              <tr>
+                <td className="ProfileTdTitle">Username :</td>
+                <td className="ProfileTdContent">{userData.username}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">First Name :</td>
+                <td className="ProfileTdContent">{userData.name.firstName}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Middle Name :</td>
+                <td className="ProfileTdContent">{userData.name.middleName}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Last Name :</td>
+                <td className="ProfileTdContent">{userData.name.lastName}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">PRN :</td>
+                <td className="ProfileTdContent">{userData.PRN}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Email :</td>
+                <td className="ProfileTdContent">{userData.email}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Phone :</td>
+                <td className="ProfileTdContent">{userData.contact}</td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Resources uploaded :</td>
+                <td className="ProfileTdContent">
+                  {userData.numberOfResourceUploaded}
+                </td>
+              </tr>
+              <br />
+              <br />
+
+              <tr>
+                <td className="ProfileTdTitle">Department :</td>
+                <td className="ProfileTdContent">{userData.department}</td>
+              </tr>
+              <br />
+              <br />
+            </table>
+
+            <br />
+          </div>
         </div>
-        <br />
-        <div className="profileHeading">
-          <h5>
-            <br />
-            First Name <br />
-            <br />
-            <br />
-            <br />
-            Last Name <br />
-            <br />
-            <br />
-            <br />
-            PRN <br />
-            <br />
-            <br />
-            <br />
-            Email <br />
-            <br />
-            <br />
-            <br />
-            Phone <br />
-            <br />
-            <br />
-            <br />
-            Resources Uploaded <br />
-            <br />
-            <br />
-            <br />
-            Branch <br />
-            <br />
-            <br />
-          </h5>
-        </div>
-        <div className="profileContent">
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            Nikhil
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            Danapgol
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            2019BTECS00036
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            nikhil.danapgol@walchandsangli.ac.in
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            9529172766
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            5
-          </h5>
-          <h5
-            style={{
-              borderStyle: "ridge ",
-              width: "75%",
-              display: "relative",
-              padding: "10px"
-            }}
-          >
-            CSE
-          </h5>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
