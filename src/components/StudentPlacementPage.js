57dc1b53708f8174b0cfbd26d5226d2548dac0c7
Harshal Kodgire <84096031+Harshal-9@users.noreply.github.com>
2021-11-17 13:42:19 +0530

    initial commit

--- src/components/StudentPlacementPage.js
+++ src/components/StudentPlacementPage.js
@@ -1,6 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import { Redirect } from "react-router-dom";
+import axios from "axios";
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
 
 function StudentPlacementPage() {
   function handleViewInsights() {
@@ -8,16 +34,63 @@ function StudentPlacementPage() {
   }
 
   const [click, setClick] = useState(false);
+  const [role, setRole] = useState("");
+  const [notificationArray, setNotificationArray] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://afternoon-ocean-57702.herokuapp.com/login", {
+        withCredentials: true
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.data.loggedIn === false) setRole("invalid");
+        else {
+          setRole(res.data.decodedData.role);
+          axios
+            .get("https://afternoon-ocean-57702.herokuapp.com/notification")
+            .then((res) => {
+              console.log(res);
+              let receivedData = res.data;
+              receivedData.reverse();
+              for (let i = 0; i < receivedData.length; i++) {
+                setNotificationArray((arr) =>
+                  arr.concat(
+                    <div>
+                      <p
+                        style={{ display: "inline-block", marginBottom: "2px" }}
+                      >
+                        <b>{receivedData[i].message} </b>
+                        <small style={{ fontSize: "0.9rem", color: "grey" }}>
+                          {timeSince(receivedData[i].time)}
+                        </small>
+                      </p>
+                    </div>
+                  )
+                );
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err.message);
+        setRole("invalid");
+      });
+  }, []);
+
   return (
     <div>
       <Sidebar />
       <div className="content">
         <div style={{ textAlign: "center", color: "red", fontSize: "20px" }}>
-          <p>This is Notification 1</p>
-          <p>This is Notification 2</p>
-          <p>This is Notification 3</p>
-          <p>This is Notification 4</p>
+          {notificationArray}
+          <br />
           <hr />
+          <br />
           <button onClick={handleViewInsights}>View Placement Insights</button>
           {click ? <Redirect to="/TPOPage/TPOInsightsView/student" /> : null}
         </div>
--- src/components/TPO/TPONotification.js
+++ src/components/TPO/TPONotification.js
@@ -1,24 +1,158 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./TPOSlideBar";
+import axios from "axios";
 
 function Notification() {
+  function sendNotification(event) {
+    event.preventDefault();
+    console.log("Submit", event.target.message.value, Date.now());
+
+    const fd = new FormData();
+    fd.append("message", event.target.message.value);
+    fd.append("time", Date.now());
+
+    axios
+      .post("https://afternoon-ocean-57702.herokuapp.com/notification", fd)
+      .then((res) => {
+        console.log(res);
+        alert("Notification added successfully !");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
+
+  function handleDelete(mongoId) {
+    console.log(mongoId);
+
+    axios
+      .post(
+        "https://afternoon-ocean-57702.herokuapp.com/notification/delete/" +
+          mongoId
+      )
+      .then((res) => {
+        console.log(res);
+        alert("Notification deleted successfully");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const [role, setRole] = useState("");
+  const [notificationArray, setNotificationArray] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://afternoon-ocean-57702.herokuapp.com/login", {
+        withCredentials: true
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.data.loggedIn === false) setRole("invalid");
+        else {
+          setRole(res.data.decodedData.role);
+          axios
+            .get("https://afternoon-ocean-57702.herokuapp.com/notification")
+            .then((res) => {
+              console.log(res);
+              let receivedData = res.data;
+              for (let i = 0; i < receivedData.length; i++) {
+                setNotificationArray((arr) =>
+                  arr.concat(
+                    <div>
+                      <div style={{ textAlign: "left", color: "indigo" }}>
+                        <p style={{ display: "inline-block" }}>
+                          <b>{receivedData[i].message} </b>
+                          <small style={{ fontSize: "0.9rem", color: "grey" }}>
+                            {timeSince(receivedData[i].time)}
+                          </small>
+                        </p>
+                        <button
+                          onClick={() => {
+                            handleDelete(receivedData[i]._id);
+                          }}
+                          style={{
+                            textAlign: "right",
+                            height: "20px",
+                            padding: "2px"
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  )
+                );
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err.message);
+        setRole("invalid");
+      });
+  }, []);
+
   return (
     <div>
       <Sidebar />
-      <div className="content">
-        <h1 style={{ align: "center" }}>Welcome to Notification page</h1>
+      <div className="content" style={{ textAlign: "center" }}>
+        <h1 style={{ textAlign: "center" }}>Welcome to Notification page</h1>
         <br />
-        <label style={{ textAlign: "center" }}>Enter new notification</label>
+        <form onSubmit={sendNotification}>
+          <label style={{ textAlign: "center" }}>Add new notification</label>
+          <br />
+          <br />
+          <textarea
+            name="message"
+            required
+            row="30"
+            cols="30"
+            style={{ resize: "none", width: "500px", height: "100px" }}
+          ></textarea>
+          <br />
+          <br />
+          <button>Add new Notification</button>
+        </form>
         <br />
-        <br />
-        <textarea
-          row="30"
-          cols="30"
-          style={{ resize: "none", width: "300px", height: "100px" }}
-        ></textarea>
+        <hr />
+        <h1>Previous notification</h1>
+        {notificationArray}
       </div>
     </div>
   );
 }
 
 export default Notification;
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
